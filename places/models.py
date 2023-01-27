f99f8340089d42de7aae3772c1432dff57832269
mavel <p.m.berdnikov@gmail.com>
2023-01-28 01:21:56 +0300

    add Place model

--- places/models.py
+++ places/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Place(models.Model):
+    title = models.CharField('название', max_length=200)
+    description_short = models.TextField('краткое описание')
+    description_long = models.TextField('длинное описание')
+    lat = models.FloatField()
+    lon = models.FloatField()
+
+    def __str__(self):
+        return self.title
+
