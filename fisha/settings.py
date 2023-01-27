3b8a877df8a0c703fb8e1e9e2f3dd2ace2787ca0
mavel <p.m.berdnikov@gmail.com>
2023-01-28 01:05:52 +0300

    rename app

--- fisha/settings.py
+++ fisha/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'fisha_map'
+    'places'
 ]
 
 MIDDLEWARE = [
--- places/apps.py
+++ places/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class FishaMapConfig(AppConfig):
+class PlacesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'fisha_map'
+    name = 'places'
