cc06275588f7eab4f48c5fe4eeb02916280d647d
mavel <p.m.berdnikov@gmail.com>
2023-01-28 18:27:49 +0300

    Move data from template to view

--- places/models.py
+++ places/models.py
@@ -6,7 +6,8 @@ class Place(models.Model):
     description_short = models.TextField('краткое описание')
     description_long = models.TextField('длинное описание')
     lat = models.FloatField()
-    lon = models.FloatField()
+    lng = models.FloatField()
+    slug = models.SlugField(unique=True)
 
     def __str__(self):
         return self.title
@@ -16,6 +17,10 @@ class PlaceImage(models.Model):
     place = models.ForeignKey(Place, on_delete=models.CASCADE, verbose_name='место',
                               related_name='images', related_query_name='image')
     image = models.ImageField('картинка')
+    position = models.IntegerField('порядковый номер', null=True, blank=True)
+
+    class Meta:
+        unique_together = ['place', 'position']
 
     def __str__(self):
-        return f'{self.id}: {self.place.title}'
+        return f'{self.position or 0}: {self.place.title}'
--- places/views.py
+++ places/views.py
@@ -1,5 +1,29 @@
 from django.shortcuts import render
 
+from .models import Place
+
 
 def index(request):
-    return render(request, 'index.html')
+    places = Place.objects.all()
+
+    feature_collection = {
+      "type": "FeatureCollection",
+      "features": []
+    }
+
+    for place in places:
+        feature_collection['features'].append({
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [place.lat, place.lng]
+            },
+            "properties": {
+                "title": place.title,
+                "placeId": place.slug,
+                "detailsUrl": f"https://raw.githubusercontent.com/devmanorg/where-to-go-frontend/master/places/{place.slug}.json"
+            }
+        })
+
+    context = {'feature_collection': feature_collection}
+    return render(request, 'index.html', context=context)
